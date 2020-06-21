63bf55a1ce7679b4cc15b4076e415112c3c9c369
carlosprietobarron <carlosprietobarron@gmail.com>
2020-06-21 09:02:26 -0700

    Add menu for user session control

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+
+    def sign_up
+      if user_signed_in?
+        link_to "Sign up", new_user_registration_path, class: "nav-link disabled"
+      else
+        link_to "Sign up", new_user_registration_path, class: "nav-link"
+      end
+    end
+
+    def sign_in
+        if user_signed_in?
+          link_to "Sign in", new_user_session_path, class: "nav-link disabled"
+        else
+          link_to "Sign in", new_user_session_path, class: "nav-link"
+        end
+      end
+
+      def sign_out
+        if !user_signed_in?
+          link_to "Sign out", destroy_user_session_path, class: "nav-link disabled"
+        else
+          link_to "Sign out", destroy_user_session_path, :method => :delete,  class: "nav-link"
+        end
+      end
+
 end

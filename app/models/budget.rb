92fece2fad777c6889f44fb3758b625a8c4ffe43
carlosprietobarron <carlosprietobarron@gmail.com>
2020-06-25 15:09:42 -0700

    repair budget model

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,7 +1,5 @@
 class Budget
     validate :budget, presence: true
-    validate :user_id uniqueness: { scope: :group_id,
-            message: "should happen once per year" }
     belongs_to :user, :foreign_key => 'user_id'
     belongs_to :group, :foreign_key => 'group_id'
 end

cc99fc79b8523f611e464dd09f43dd00f17af745
carlosprietobarron <carlosprietobarron@gmail.com>
2020-06-25 15:38:00 -0700

    correct heroku error

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,4 +1,4 @@
-class Budget
+class Budget < ApplicationRecord
     validate :budget, presence: true
     belongs_to :user, :foreign_key => 'user_id'
     belongs_to :group, :foreign_key => 'group_id'

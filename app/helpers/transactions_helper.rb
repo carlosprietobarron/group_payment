1e829240b610155319ca46ad9f9efb4870f2a80b
carlosprietobarron <carlosprietobarron@gmail.com>
2020-06-29 10:42:22 -0700

    Eliminate logic from views

--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -3,6 +3,12 @@ module TransactionsHelper
     link_to 'Edit', edit_transaction_path(Transaction.find(trans_id)) if current_user.id == author
   end
 
+  def get_icon(transaction)
+    if !transaction.group.nil? 
+      image_tag  "#{transaction.group.icon}"
+    end
+  end
+
   # rubocop:disable Layout/LineLength
   def delete_btn(author, trans_id)
     link_to 'Destroy', Transaction.find(trans_id), method: :delete, data: { confirm: 'Are you sure?' } if current_user.id == author

670619a01a99274d80279c46c3d929e7a6d3f7aa
carlosprietobarron <carlosprietobarron@gmail.com>
2020-06-30 13:20:30 -0700

    Clean logic from group index

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -2,16 +2,18 @@ module GroupsHelper
   def group_list(groups)
     @content = content_tag(:span, '')
     groups.collect do |c|
-      @content.concat(image_tag(c.icon.to_s))
-      @content.concat(content_tag(:span, c.name))
-      @content.concat(content_tag(:a, href: "groups/#{c.id}") do
-                        'show'
-                      end)
-      @content.concat(content_tag(:a, href: 'group', method: :delete, data: { confirm: 'Are you sure?' }) do
-                        'delete'
-                      end)
+      @content.concat(
+                  content_tag(:div, :class => "group-card") do
+                    concat(image_tag(c.icon.to_s) + " " +
+                           content_tag(:span, c.name) + " " +
+                           content_tag(:a, href: "groups/#{c.id}") do
+                                'show'
+                           end 
+                    )                    
+                  end
+                )
     end
-    @content.concat(:br)
+
     @content
   end
 end

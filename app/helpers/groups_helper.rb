bd35483348efa60a1e742a879190fdab67f8f396
carlosprietobarron <carlosprietobarron@gmail.com>
2020-07-01 09:38:19 -0700

    Fix linter rubocop errors

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -16,29 +16,29 @@ module GroupsHelper
   end
 
   def select_icon
-    icons = {"car" => "icons/car.png", \
-      "travel" => "icons/case.png", \
-      "clothes" => "icons/clothes.png", \
-      "grocery" => "icons/grocery.png", \
-      "home" => "icons/home.png", \
-      "office" => "icons/office.png", \
-      "transport" => "icons/transport.png"}
+    icons = { 'car' => 'icons/car.png', \
+              'travel' => 'icons/case.png', \
+              'clothes' => 'icons/clothes.png', \
+              'grocery' => 'icons/grocery.png', \
+              'home' => 'icons/home.png', \
+              'office' => 'icons/office.png', \
+              'transport' => 'icons/transport.png' }
 
-    @content = content_tag(:span, "")
-    
+    @content = content_tag(:span, '')
+
     icons.each do |icon|
       @content.concat(
-        content_tag(:div, class: "form-radio-btn-item") do
-            concat(
-              content_tag(:div, class: "card-body-rad form-inline card-radio-btn") do
-                radio_button_tag(:icon, icon[1])+ " " +
-                image_tag("#{icon[1]}", class: "card-img")
-              end
-            )
-          end
-      ) #content.concat
+        content_tag(:div, class: 'form-radio-btn-item') do
+          concat(
+            content_tag(:div, class: 'card-body-rad form-inline card-radio-btn') do
+              radio_button_tag(:icon, icon[1]) + ' ' +
+              image_tag((icon[1]).to_s, class: 'card-img')
+            end
+          )
+        end
+      ) # content.concat
     end
-   
+
     @content
   end
 end
--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -6,8 +6,6 @@ module TransactionsHelper
   def get_icon(transaction)
     image_tag transaction.group.icon.to_s unless transaction.group.nil?
   end
-  
-
 
   # rubocop:disable Layout/LineLength
   def delete_btn(author, trans_id)
